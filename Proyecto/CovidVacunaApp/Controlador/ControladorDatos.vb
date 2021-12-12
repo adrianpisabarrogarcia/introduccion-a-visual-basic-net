89ab4c6165392261c80b0f66a6708202065094af
adrianpisabarrogarcia <adrian.pisabarro.garcia@gmail.com>
2021-12-12 22:06:23 +0100

    archivos json con datos incluidos

--- Proyecto/CovidVacunaApp/Controlador/ControladorDatos.vb
+++ Proyecto/CovidVacunaApp/Controlador/ControladorDatos.vb
@@ -2,6 +2,49 @@
 
 
     Public Sub descargarArchivos()
+        Dim thisDate As Date
+        thisDate = #1/1/2021#
+        Dim days As Integer
+        days = DateDiff("d", thisDate, Now)
+
+        Dim texto As String = ""
+
+        For index As Integer = 0 To days
+            Dim dia As String = CStr(Format(thisDate.AddDays(index), "yyyyMMdd"))
+
+            Try
+                Dim url As String = "https://covid-vacuna.app/data/" & dia & ".json"
+                Dim rutaDescarga As String = "C:\Users\apisabarro\Desktop\Git\introduccion-a-visual-basic-net\Proyecto\CovidVacunaApp\Datos\" & dia & ".json"
+
+                If Not My.Computer.FileSystem.FileExists(rutaDescarga) Then
+                    My.Computer.Network.DownloadFile(url, rutaDescarga)
+                End If
+
+            Catch ex As Exception
+                'Para un futuro estaría bien implementar un fichero que marque que ficheros no se han descargado
+                'Pero por el momento no vamos a añadir ninguna excepción
+            End Try
+        Next
+
+        Dim todayDate As Date
+        todayDate = Now
+
+        Try
+            Dim url As String = "https://covid-vacuna.app/data/latest.json"
+            Dim rutaDescarga As String = "C:\Users\apisabarro\Desktop\Git\introduccion-a-visual-basic-net\Proyecto\CovidVacunaApp\Datos\" & CStr(Format(todayDate, "yyyyMMdd")) & ".json"
+
+            If Not My.Computer.FileSystem.FileExists(rutaDescarga) Then
+                My.Computer.Network.DownloadFile(url, rutaDescarga)
+            End If
+
+
+        Catch ex As Exception
+            'Para un futuro estaría bien implementar un fichero que marque que ficheros no se han descargado
+            'Pero por el momento no vamos a añadir ninguna excepción
+        End Try
+
+
+        MessageBox.Show("Proceso terminado.")
 
     End Sub
 
